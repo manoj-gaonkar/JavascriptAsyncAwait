bd0d4f0bb0bf7901c24baa6f85523f64648421d5
manoj <manojgaonkar7648@gmail.com>
2024-02-21 23:15:24 +0530

    Promises in JS

--- script.js
+++ script.js
@@ -26,15 +26,45 @@
 
 // ----------CALLBACK FUNCTIONS -----------
 
-function called(name,calledCallBack){
-    setTimeout(()=>{
-        console.log(name);
-        if(calledCallBack) calledCallBack();
-    },2000)
+// function called(name,calledCallBack){
+//     setTimeout(()=>{
+//         console.log(name);
+//         if(calledCallBack) calledCallBack();
+//     },2000)
+// }
+
+// called("data is being retrived",()=>{
+//     called("still retriving data",() => {
+//         called("Timeout")
+//     });
+// })
+
+
+// --------------- PROMISES -------------
+
+const testpromise = new Promise((res,rej)=>{
+    console.log("hi");
+})
+
+
+async function addFive(a){
+    return new Promise((res,rej) => {
+        setTimeout(()=>{
+            if(a){
+                if(Number.isInteger(a)) res(a+5)
+                else rej("a is not an integer")
+            }
+            else{
+                rej("this function takes an integer arguement")
+            }
+        },2000)
+    })
 }
 
-called("data is being retrived",()=>{
-    called("still retriving data",() => {
-        called("Timeout")
-    });
-})
+const pr =  addFive(8);
+
+
+// this thing runs for resolve
+pr.then((res) => console.log(res,"this is res"))
+// this thing runs when there is reject returned ie. ERROR
+pr.catch((err) => console.log("ERROR: ",err))
