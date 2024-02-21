9e8f7c34c077504273f1dbd4d89a4bd8afed8910
manoj <manojgaonkar7648@gmail.com>
2024-02-21 22:31:21 +0530

    Callback function JS

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
-const delay = ms => new Promise(res => setTimeout(res, ms));
-const getFruit = async (name) => {
-    fruits = {
-        "a":"apple",
-        "b":"Mango",
-        "c":"Banana"
-    }
+// const delay = ms => new Promise(res => setTimeout(res, ms));
+// const getFruit = async (name) => {
+//     fruits = {
+//         "a":"apple",
+//         "b":"Mango",
+//         "c":"Banana"
+//     }
    
-    await delay(3000)
-    return fruits[name]
+//     await delay(3000)
+//     return fruits[name]
+// }
+
+// console.log(getFruit("a"));
+
+// async function getFruits(){
+//     const a = await getFruit("a");
+//     console.log(a);
+
+//     const b = await getFruit("b");
+//     console.log(b);
+
+// }
+
+// console.log(getFruits());
+
+
+// ----------CALLBACK FUNCTIONS -----------
+
+function called(name,calledCallBack){
+    setTimeout(()=>{
+        console.log(name);
+        if(calledCallBack) calledCallBack();
+    },2000)
 }
 
-console.log(getFruit("a"));
-
-async function getFruits(){
-    const a = await getFruit("a");
-    console.log(a);
-
-    const b = await getFruit("b");
-    console.log(b);
-
-}
-
-console.log(getFruits());
+called("data is being retrived",()=>{
+    called("still retriving data",() => {
+        called("Timeout")
+    });
+})
